9f2e5b6f0799d5e7813669c6315738a12c08c94d
Robin Huiser <robin@rdc.pt>
2021-05-14 18:48:53 +0100

    Fixed intentional bug

--- naming/naming_test.go
+++ naming/naming_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestColor(t *testing.T) {
 	arg := "blue"
-	want := "#0000FE"
+	want := "#0000FF"
 	got := Color(arg)
 	if got != want {
 		t.Errorf("Color(%q) = %s; want %s", arg, got, want)
